88c40c2cb015f72a3ddb78d02ba46e32cb3e7ded
Mikko Lehtonen <scoopr@iki.fi>
2020-05-15 00:19:21 +0300

    Initial import

---
+++ src/async_block.rs
@@ -0,0 +1,18 @@
+
+#[cfg(not(target_arch = "wasm32"))]
+use futures;
+use std::future::Future;
+
+/// Executes a `async fn` and blocks on it.
+/// Works for native and web.
+pub fn block_on<F: Future<Output=()> + 'static >(func: impl FnOnce() -> F)
+{
+    #[cfg(not(target_arch = "wasm32"))]
+    {
+        futures::executor::block_on(func());
+    }
+    #[cfg(target_arch = "wasm32")]
+    {
+        wasm_bindgen_futures::spawn_local(func());
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,3 @@
+
+mod async_block;
+pub use async_block::block_on;
