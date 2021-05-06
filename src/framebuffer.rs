ce63c6c727461c232c6539c915d304f4eadd05ef
Mikko Lehtonen <scoopr@iki.fi>
2021-05-06 21:36:38 +0300

    wgpu update

--- src/framebuffer.rs
+++ src/framebuffer.rs
@@ -282,7 +282,7 @@ impl Framebuffer {
                         dimension: Some(wgpu::TextureViewDimension::D2),
                         aspect: wgpu::TextureAspect::All,
                         base_mip_level: 0,
-                        level_count: None,
+                        mip_level_count: None,
                         base_array_layer: 0,
                         array_layer_count: None,
                     });
