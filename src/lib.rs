270979cebd04e2817d1a3557071792affed9011d
Mikko Lehtonen <scoopr@iki.fi>
2021-05-06 23:13:41 +0300

    Split to features

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,14 @@
+#[cfg(feature = "async")]
 mod async_block;
+
+#[cfg(feature = "framebuffer")]
 mod framebuffer;
 
 /// Re-export the dependent wgpu version, for easily using the same version
 pub use wgpu;
 
+#[cfg(feature = "async")]
 pub use async_block::block_on;
+
+#[cfg(feature = "framebuffer")]
 pub use framebuffer::Framebuffer;
